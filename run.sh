fb6fa93ed48f8eca8ecbbc7e47a29c9b783de5a9
Bean, Andrew K <andan@bsg.ac.uk>
2021-08-25 15:16:33 +0100

    fix: add rewrite for local api url

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@ curl $OPENAPI_PATH > ./specs/swagger.json
 
 cat ./specs/swagger.json
 
-./dapperdox --spec-rewrite-url=#/components=#/definitions -bind-addr 0.0.0.0:3123 -spec-dir ./specs/ -site-url http://localhost:3000 -assets-dir ./content -force-specification-list true -theme=dapperdox-theme-maps -spec-rewrite-url=http://localhost:3123/ -theme-dir ./themes/ 
+./dapperdox 
+ -spec-rewrite-url=#/components=#/definitions
+ -bind-addr 0.0.0.0:3123 
+ -spec-dir ./specs/ 
+ -site-url http://localhost:3000 
+ -assets-dir ./content 
+ -force-specification-list true 
+ -theme=dapperdox-theme-maps 
+ -spec-rewrite-url=http://localhost:3000=$API_PUBLIC_URL
+ -theme-dir ./themes/ 
 
 
